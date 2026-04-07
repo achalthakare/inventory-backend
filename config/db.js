dc6ebee7c99325892d150294baaf3738204f8711
Achal Thakare <achalthakare123@gmail.com>
2026-04-08 00:44:31 +0530

    switched to MYSQL_URL

--- config/db.js
+++ config/db.js
@@ -12,18 +12,26 @@
 
 
 
+// const mysql = require('mysql2');
+// require('dotenv').config();
+
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT, // ✅ IMPORTANT for Railway
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+// module.exports = db;
+
+
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT, // ✅ IMPORTANT for Railway
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+const db = mysql.createPool(process.env.MYSQL_URL);
 
 module.exports = db;
