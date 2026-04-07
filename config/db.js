8c6128237e3542c69b0dbf1b3499e4d178d43e52
Achal Thakare <achalthakare123@gmail.com>
2026-04-08 00:22:22 +0530

    updated backend config

--- config/db.js
+++ config/db.js
@@ -1,3 +1,17 @@
+// const mysql = require('mysql2');
+// require('dotenv').config();
+
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME
+// });
+
+// module.exports = db;
+
+
+
 const mysql = require('mysql2');
 require('dotenv').config();
 
@@ -5,7 +19,11 @@ const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT, // ✅ IMPORTANT for Railway
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = db;
